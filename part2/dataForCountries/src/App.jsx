0d59a923115260891db22a581a966d4ac8be6ff2
Doctor3131 <sirielwnf@gmail.com>
2025-03-10 23:12:30 +0700

    part 2.19 done

--- part2/dataForCountries/src/App.jsx
+++ part2/dataForCountries/src/App.jsx
@@ -1,54 +1,15 @@
 import { useState, useEffect } from 'react'
 import axios from 'axios'
+import ShowAll from './components/ShowAll'
+import ShowOnePerson from './components/ShowOnePerson'
+import ShowData from './components/ShowData'
 
-const ShowAll = ({showResult}) => {
-  return (
-    <ul>
-    {showResult.map(eachData => (
-      <li key={eachData.name.common}>
-        {eachData.name.common}
-      </li>
-    ))}
-    </ul>
-  )
-}
-
-const ShowOnePerson = ({country}) => {
-
-  const flagStyle = {
-    fontSize: "250px",
-    lineHeight: '1',
-    marginTop: '20px',
-    
-  }
-
-  return (
-    <>
-      {/* {console.log(country.name)} */}
-      <h1>{country.name.common}</h1>
-      
-      Capital {country.capital}<br />
-      Area {country.area}
-
-      <h2>Languages</h2>
-      <ul>
-        {Object.entries(country.languages).map(([code, name]) => (
-          <li key={code}>{name}</li>
-        ))}
-      </ul>
-      {console.log(country.flag)}
-      <div style={flagStyle}>
-        {country.flag}
-      </div>
-    </>
-  )
-}
 
 const App = () => {
   const [data, setData] = useState([])
   const [name, setName] = useState('')
   const [countriesName, setCountriesName] = useState(null)
-
+  const [shown, setShown] = useState(null)
 
   useEffect(() => {
     if (countriesName) {
@@ -71,20 +32,23 @@ const App = () => {
     setCountriesName(name)
   }
 
+  const toggleShown = (countryName) => (
+    shown === countryName 
+    ? setShown(null)
+    : setShown(countryName)
+  ) 
+
   return (
     <div>
       <form onSubmit={onSearch}>
         find countries: <input value={name}
-                  onChange={handleNameChange}/>
+                               onChange={handleNameChange}/>
       </form>
-      {console.log(data)}
-      {data.length > 10 ? 
-        <p>Too many matches, specify another filter</p>
-       : data.length === 1 ? 
-        <ShowOnePerson country={data[0]}/>
-       : 
-        <ShowAll showResult={data}/>
-      }
+      <ShowData data={data}
+                ShowAll={ShowAll}
+                ShowOnePerson={ShowOnePerson}
+                shown={shown}
+                toggleShown={toggleShown}/>
     </div>
   )
 }
