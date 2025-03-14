0ea3e0b3789ed67056a1ff1bfd6e0a2d8c7f7fb4
Doctor3131 <sirielwnf@gmail.com>
2025-03-14 11:37:13 +0700

    just a daily commit

--- part2/dataForCountriesAnswer/src/main.jsx
+++
@@ -1,6 +0,0 @@
-import ReactDOM from 'react-dom/client'
-import App from './App'
-
-ReactDOM.createRoot(document.getElementById('root')).render(<App />)
-
-// export VITE_SOME_KEY=6e07b4008e74e7349e7c7f050f6019ed && npm run dev
