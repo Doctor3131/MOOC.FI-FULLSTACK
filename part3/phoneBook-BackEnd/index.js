40d3cf12649105a9b5e6c2494bd0d761c440dc34
Doctor3131 <sirielwnf@gmail.com>
2025-04-05 16:15:24 +0700

    part 3.4 done

--- part3/phoneBook-BackEnd/index.js
+++ part3/phoneBook-BackEnd/index.js
@@ -36,7 +36,7 @@ app.get('/api/info', (request, response) => {
                    <p>${date.toString()}</p>`)
 })
 
-app.get('/api/persons', (request, response) => {
+app.get('/api/persons/', (request, response) => {
     response.json(persons)
 })
 
@@ -50,6 +50,13 @@ app.get('/api/persons/:id', (request, response) => {
   } else {
     response.status(404).end()
   }
+})
+
+app.delete('/api/persons/:id', (request, response) => {
+  const id = request.params.id
+  persons = persons.filter(person => person.id !== id)
+
+  response.status(204).end()
 })
 
 const PORT = 3001
