dcdf428809a3985f5949f266a8e6a2ee0ac9fd9a
Doctor3131 <sirielwnf@gmail.com>
2025-04-27 14:09:22 +0700

    yey

--- part3/coba-coba-backend/index.js
+++ part3/coba-coba-backend/index.js
@@ -14,8 +14,6 @@ const noteSchema = new mongoose.Schema({
     important: Boolean,
 })
 
-const Note = mongoose.model('Note', noteSchema)
-
 noteSchema.set('toJSON', {
   transform: (document, returnedObject) => {
     returnedObject.id = returnedObject._id.toString()
@@ -68,14 +66,9 @@ app.get('/api/notes', (request, response) => {
 })
 
 app.get('/api/notes/:id', (request, response) => {
-  const id = request.params.id
-  const note = notes.find(note => note.id === id)
-
-  if (note) {
+  Note.findById(request.params.id).then(note => {
     response.json(note)
-  } else {
-    response.status(404).end()
-  }
+  })
 })
 
 const generateId = () => {
@@ -90,19 +83,17 @@ app.post('/api/notes', (request, response) => {
   const body = request.body
 
   if (!body.content) {
-    return response.status(400).json({
-      error: 'content missing'
-    })
+    return response.status(400).json({ error: 'content missing' })
   }
 
-  const note = {
+  const note = new Note({
     content: body.content,
     important: body.important || false,
-    id: generateId(),
-  }
+  })
 
-  notes = notes.concat(note)
-  response.json(note)
+  note.save().then(savedNote => {
+    response.json(savedNote)
+  })
 })
 
 app.delete('/api/notes/:id', (request, response) => {
