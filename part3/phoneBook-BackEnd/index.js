c361ec9c47f7391884d69df99da1f409040897ac
Doctor3131 <sirielwnf@gmail.com>
2025-04-30 22:08:41 +0700

    part3.12 done

--- part3/phoneBook-BackEnd/index.js
+++ part3/phoneBook-BackEnd/index.js
@@ -1,62 +1,63 @@
+require('dotenv').config()
 const express = require('express')
 const morgan = require('morgan')
-
 const app = express()
+const Person = require('./models/person')
+
 app.use(express.json())
 app.use(express.static("dist"))
 
 morgan.token('body', (request) => JSON.stringify(request.body))
 app.use(morgan(':method :url :status :res[content-length] - :response-time ms :body'))
 
-let persons = [
-    { 
-      "id": "1",
-      "name": "Arto Hellas", 
-      "number": "040-123456"
-    },
-    { 
-      "id": "2",
-      "name": "Ada Lovelace", 
-      "number": "39-44-5323523"
-    },
-    { 
-      "id": "3",
-      "name": "Dan Abramov", 
-      "number": "12-43-234345"
-    },
-    { 
-      "id": "4",
-      "name": "Mary Poppendieck", 
-      "number": "39-23-6423122"
-    }
-]
+// let persons = [
+//     { 
+//       "id": "1",
+//       "name": "Arto Hellas", 
+//       "number": "040-123456"
+//     },
+//     { 
+//       "id": "2",
+//       "name": "Ada Lovelace", 
+//       "number": "39-44-5323523"
+//     },
+//     { 
+//       "id": "3",
+//       "name": "Dan Abramov", 
+//       "number": "12-43-234345"
+//     },
+//     { 
+//       "id": "4",
+//       "name": "Mary Poppendieck", 
+//       "number": "39-23-6423122"
+//     }
+// ]
 
 app.get('/', (request, response) => {
     response.send('<h1>Hello world</h1>')
 })
 
 app.get('/api/info', (request, response) => {
-    const total = persons.length
-    const date = new Date()
-
-    response.send(`<p>Phonebook has info for ${total} people</p>
-                   <p>${date.toString()}</p>`)
+    Person.find({})
+      .then(persons => {
+        const total = persons.length
+        const date = new Date()
+        response.send(`<p>Phonebook has info for ${total} people</p>
+          <p>${date.toString()}</p>`)
+      })
 })
 
 app.get('/api/persons', (request, response) => {
+  Person.find({}).then(persons => {
     response.json(persons)
+  })
 })
 
 
 app.get('/api/persons/:id', (request, response) => {
-  const id = request.params.id
-  const person = persons.find(person => person.id === id)
-
-  if (person) {
+  Person.findById(request.params.id).then(person => {
     response.json(person)
-  } else {
-    response.status(404).end()
-  }
+  })
 })
 
 app.delete('/api/persons/:id', (request, response) => {
@@ -111,7 +112,7 @@ app.post('/api/persons', (request, response) => {
   response.json(person)
 })
 
-const PORT = 3001
+const PORT = process.env.PORT
 app.listen(PORT, () => {
     console.log(`Server running on port ${PORT}`);
 })
