94bd4479fd2ae00f8507ed8cdc281679f132f516
Doctor3131 <sirielwnf@gmail.com>
2025-07-31 10:25:54 +0700

    add to my secondary laptop

--- part4/Notes-Backend/app.js
+++ part4/Notes-Backend/app.js
@@ -10,22 +10,21 @@ const app = express()
 logger.info('connecting to', config.MONGODB_URI)
 
 mongoose
-    .connect(config.MONGODB_URI)
-    .then(() => {
-        logger.info('connected to MongoDB')
-    })
-    .catch((error) => {
-        logger.error('error connection to MongoDB:', error.message)
-    })
+  .connect(config.MONGODB_URI)
+  .then(() => {
+    logger.info('connected to MongoDB')
+  })
+  .catch((error) => {
+    logger.error('error connection to MongoDB:', error.message)
+  })
 
 app.use(express.static('dist'))
 app.use(express.json())
 app.use(middleware.requestLogger)
 
-app.use('api/notes', notesRouter)
+app.use('/api/notes', notesRouter)
 
 app.use(middleware.unknownEndpoint)
 app.use(middleware.errorHandler)
 
 module.exports = app
-
