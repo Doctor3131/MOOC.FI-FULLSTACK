87d77d21a2e1a36f25e7a31ae84eb5bcc2fbac71
Doctor3131 <sirielwnf@gmail.com>
2025-08-06 13:33:36 +0700

    add to my secondary laptop

---
+++ part4/BlogList-Backend/index.js
@@ -0,0 +1,23 @@
+const express = require('express')
+const mongoose = require('mongoose')
+
+const app = express()
+
+const blogSchema = mongoose.Schema({
+  title: String,
+  author: String,
+  url: String,
+  likes: Number,
+})
+
+const Blog = mongoose.model('Blog', blogSchema)
+
+const mongoUrl = ''
+mongoose.connect(mongoUrl)
+
+app.use(express.json())
+
+
+
+
+
--- part4/Notes-Backend/controllers/notes.js
+++ part4/Notes-Backend/controllers/notes.js
@@ -2,9 +2,7 @@ const notesRouter = require('express').Router()
 const Note = require('../models/note')
 
 notesRouter.get('/', (request, response) => {
-  Note.find({}).then(notes => {
-    response.json(notes)
-  })
+  response('<h1>Hello World</h1>')
 })
 
 notesRouter.get('/:id', (request, response, next) => {
