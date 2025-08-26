3a5487c06b697a35c745b69f4ef66713d1824a9e
Doctor3131 <sirielwnf@gmail.com>
2025-08-26 08:47:35 +0700

    part4.5 done

--- part4/BlogList-Backend/tests/dummy.test.js
+++ part4/BlogList-Backend/tests/dummy.test.js
@@ -87,3 +87,45 @@ describe('total likes', () => {
     assert.strictEqual(result, 36)
   })
 })
+
+describe('favourite blog', () => {
+  const listWithMultipleBlogs = [
+    {
+      _id: '5a422a851b54a676234d17f7',
+      title: 'React patterns',
+      author: 'Michael Chan',
+      url: 'https://reactpatterns.com/',
+      likes: 7,
+      __v: 0
+    },
+    {
+      _id: '5a422aa71b54a676234d17f8',
+      title: 'Go To Statement Considered Harmful',
+      author: 'Edsger W. Dijkstra',
+      url: 'http://www.u.arizona.edu/~rubinson/copyright_violations/Go_To_Considered_Harmful.html',
+      likes: 5,
+      __v: 0
+    },
+    {
+      _id: '5a422b3a1b54a676234d17f9',
+      title: 'Canonical string representation in JSON',
+      author: 'Edsger W. Dijkstra',
+      url: 'http://www.u.arizona.edu/~rubinson/copyright_violations/Go_To_Considered_Harmful.html',
+      likes: 12,
+      __v: 0
+    }
+  ]
+
+  test('most favourite blog', () => {
+    const result = listHelper.favouriteBlog(listWithMultipleBlogs)
+    const expected = {
+      _id: '5a422b3a1b54a676234d17f9',
+      title: 'Canonical string representation in JSON',
+      author: 'Edsger W. Dijkstra',
+      url: 'http://www.u.arizona.edu/~rubinson/copyright_violations/Go_To_Considered_Harmful.html',
+      likes: 12,
+      __v: 0
+    }
+    assert.deepStrictEqual(result, expected)
+  })
+})
--- part4/BlogList-Backend/utils/list_helper.js
+++ part4/BlogList-Backend/utils/list_helper.js
@@ -6,7 +6,23 @@ const totalLikes = (blogs) => {
   return blogs.reduce((sum, blog) => sum + blog.likes, 0)
 }
 
+const favouriteBlog = (blogs) => {
+  if (blogs.length === 0) {
+    return null
+  }
+
+  const favorite = blogs.reduce((favorite, blog) => {
+    if (blog.likes > favorite.likes) {
+      return blog
+    }
+    return favorite
+  }, blogs[0])
+
+  return favorite
+}
+
 module.exports = {
   dummy,
-  totalLikes
+  totalLikes,
+  favouriteBlog
 }
