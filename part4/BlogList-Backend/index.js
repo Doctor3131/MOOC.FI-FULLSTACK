1011e3a36a3196d0f7523886e746cfdd8ce11948
Doctor3131 <sirielwnf@gmail.com>
2025-10-07 22:38:19 +0700

    fresh start to part 4

--- part4/BlogList-Backend/index.js
+++
@@ -1,9 +0,0 @@
-const app = require('./app.js')
-const express = require('express')
-const mongoose = require('mongoose')
-const config = require('./utils/config.js')
-
-const PORT = config.PORT
-app.listen(PORT, () => {
-  logger.info(`Server running on port ${PORT}`)
-})
